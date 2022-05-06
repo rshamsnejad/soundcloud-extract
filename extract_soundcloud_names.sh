f10060ea6ef603d281153db3c80026619d3276ee
Robin SHAMSNEJAD <rsham@free.fr>
2022-05-06 21:51:27 +0200

    Add first script

---
+++ extract_soundcloud_names.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+urlencode () {
+    python -c "import urllib.parse as ul; print(ul.quote(\"$1\", safe=''))"
+}
+
+PLAYLIST="$1"
+#PLAYLIST="https://soundcloud.com/user-332362291/sets/set-neuro"
+PLAYLIST_ENC="https://genmp3.net/tracks.php?u=$(urlencode $PLAYLIST)"
+
+HTML_FILENAME=${PLAYLIST##*/}.html
+SONG_FILENAME=${HTML_FILENAME/html/names.txt}
+
+#google-chrome-stable --headless --dump-dom "$PLAYLIST_ENC" > $HTML_FILENAME \
+#    2>/dev/null || exit 1
+
+grep "<a href=\"#\" title=\"Download " $HTML_FILENAME \
+    | cut -d '>' -f 3 | cut -d '<' -f 1 | sed '/^[[:space:]]*$/d' \
+    > $SONG_FILENAME
+
+TOTAL=$(wc -l < $SONG_FILENAME)
+CURRENT=1
+
+while read -r name
+do
+    echo "($CURRENT/$TOTAL) Copying '$name' to clipboard..."
+    echo "$name" | xclip -selection clipboard
+    read -p "Press enter to continue" < /dev/tty
+
+    CURRENT=$((CURRENT+1))
+done < $SONG_FILENAME
